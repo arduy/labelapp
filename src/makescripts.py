0079686c214c0ec9e0023e961f0846a9a90c7d15
Adrian Duynstee <adrian.duy@gmail.com>
2019-03-18 16:59:10 -0700

    Fix path resolution

--- src/makescripts.py
+++ src/makescripts.py
@@ -3,7 +3,9 @@ import sys
 import json
 
 if __name__ == '__main__':
-    mypath = os.path.dirname(sys.argv[0])
+    # meant to be run after building with pyinstaller
+    # don't run as python script, otherwise sys.executable will point to wrong file
+    mypath = os.path.dirname(sys.executable)
     main = os.path.join(mypath, 'main.exe')
     with open(os.path.join(mypath,'scripts.json'), 'r') as read_file:
         data = json.load(read_file)
